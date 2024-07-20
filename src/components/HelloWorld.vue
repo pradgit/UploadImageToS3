d31fd2b4d0af3cc4149eea9ccc236922f4ec0abd
pradgit <phadnispradnya@gmail.com>
2024-07-20 11:33:41 +0530

    Update HelloWorld.vue
    added my new api link
    https://6ip4d3a4ug.execute-api.us-east-1.amazonaws.com/default/tryliveproject

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -26,7 +26,7 @@ const MAX_IMAGE_SIZE = 1000000
 /* ENTER YOUR ENDPOINT HERE
    FILES UPLOADED TO MY ENDPOINT ARE AUTOMATICALLY DELETED EVERY FEW HOURS */
 
-const API_ENDPOINT = 'https://c1os4xvwji.execute-api.us-east-1.amazonaws.com/default/myfirstlambda'
+const API_ENDPOINT = 'https://6ip4d3a4ug.execute-api.us-east-1.amazonaws.com/default/tryliveproject'
 
 export default {
   name: 'app',
